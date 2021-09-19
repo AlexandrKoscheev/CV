df3f430e1039e70ab99957ca92d814d8fdd31031
AlexandrKoscheev <91019008+AlexandrKoscheev@users.noreply.github.com>
2021-09-19 17:25:03 +0300

    create CV

---
+++ app.js
@@ -0,0 +1,14 @@
+let myButton = document.querySelector('.my-button')
+let myInput = document.querySelector('.my-input')
+let myHeader = document.querySelector('.header')
+let mySecondInput = document.querySelector('.input-two')
+
+
+
+myButton.addEventListener('click', function(){
+	
+	let inputValue = myInput.value
+	let secondInputValue = mySecondInput.value
+
+//	myHeader.innerText = `${secondInputValue} ${InputValue}`
+})
